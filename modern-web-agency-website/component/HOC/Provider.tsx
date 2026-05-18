638e269f1f034c01efa9d0ecca278a92bf419c95
Hosein Alipour <143794262+MadeByHosein@users.noreply.github.com>
2026-05-19 00:28:28 +0330

    Add files via upload

--- modern-web-agency-website/component/HOC/Provider.tsx
+++ modern-web-agency-website/component/HOC/Provider.tsx
@@ -1,1 +1,16 @@
+import { ThemeProvider } from 'next-themes'
+import {ReactNode} from 'react'
 
+const Provider = ({children}: {children: ReactNode }) => {
+  return (
+    <ThemeProvider
+     attribute= "class" 
+     enableSystem
+     defaultTheme='system'
+    >
+        {children}
+    </ThemeProvider>
+  )
+}
+
+export default Provider
