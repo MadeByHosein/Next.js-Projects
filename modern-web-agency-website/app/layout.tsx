3d035e6208b46ec338b5ac765a4c6a3cc1f80fc8
Hosein Alipour <143794262+MadeByHosein@users.noreply.github.com>
2026-05-19 00:25:59 +0330

    Add files via upload

---
+++ modern-web-agency-website/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Rubik } from "next/font/google";
+import "./globals.css";
+import Provider from "@/component/HOC/Provider";
+import ResponsiveNav from "@/component/Home/Navbar/ResponsiveNav";
+
+const font = Rubik({
+  weight:['300','400','500','600','700','800','900']
+})
+export const metadata: Metadata = {
+  title: "Web Agency | Next.js + TailwindCss",
+  description: "A modern web agency designed by next.js and tailwindcss",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      suppressHydrationWarning
+      lang="en"
+      className={`${font.className} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">
+        <Provider >
+          <ResponsiveNav />
+          {children}
+        
+        </Provider>
+        </body>
+    </html>
+  );
+}
---
+++ modern-web-agency-website/app/page.tsx
@@ -0,0 +1,11 @@
+import Home from '@/component/Home/Home'
+
+const page = () => {
+  return (
+    <div>
+      <Home />
+    </div>
+  )
+}
+
+export default page
